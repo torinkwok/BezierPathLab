3aade4eb0b5a6e7afc6a11e30334b3ec27eca404
Tong Guo <Tong-G@outlook.com>
2014-07-22 18:36:27 +0800

    Initial push

---
+++ BezierLab/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  BezierLab
+//
+//  Created by Tong G. on 7/22/14.
+//  Copyright (c) 2014 Tong G. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
