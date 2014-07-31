b7835dd38059f1200a0fc63504ba2dc304c6714d
Tong Guo <Tong-G@outlook.com>
2014-07-31 13:46:05 +0800

    Routine backup.

---
+++ BezierLab/BLContentView.m
@@ -0,0 +1,13 @@
+//
+//  BLContentView.m
+//  BezierLab
+//
+//  Created by Tong G. on 7/31/14.
+//  Copyright (c) 2014 Tong G. All rights reserved.
+//
+
+#import "BLContentView.h"
+
+@implementation BLContentView
+
+@end
