d24f95bfcd347c7e38d91dca0973698563ebfb98
Tong Guo <Tong-G@outlook.com>
2014-08-06 15:44:28 +0800

    Routine backup.

--- BezierLab/BLAppDelegate.m
+++ BezierLab/BLAppDelegate.m
@@ -36,6 +36,9 @@
 
 // BLAppDelegate class
 @implementation BLAppDelegate
+    {
+    NSWindow* _mainWindow;
+    }
 
 @synthesize _mainWindowController;
 
@@ -45,12 +48,54 @@
     {
     self._mainWindowController = [ BLMainWindowController mainWindowController ];
 
+    _mainWindow = [ [ self._mainWindowController window ] retain ];
+
     [ self._mainWindowController showWindow: self ];
+    }
+
+- ( void ) dealloc
+    {
+    [ _mainWindow release ];
+    _mainWindow = nil;
+
+    [ super dealloc ];
     }
 
 - ( void ) applicationDidFinishLaunching: ( NSNotification* )_Notification
     {
 
+    }
+
+- ( IBAction ) creatingAWindow: ( id )_Sender
+    {
+    NSRect contentRect = NSMakeRect( 400, 200, 400, 400 );
+    NSWindow* window = [ [ NSWindow alloc ] initWithContentRect: contentRect
+                                                        styleMask: NSTitledWindowMask | NSClosableWindowMask
+                                                          backing: NSBackingStoreBuffered
+                                                            defer: NO ];
+//    [ window display ];
+    [ window orderFront: self ];
+    }
+
+- ( IBAction ) printingWindow: ( id )_Sender
+    {
+    NSRect frame = _mainWindow.frame;
+    NSLog( @"%@", NSStringFromRect( _mainWindow.frame ) );
+    NSData* PDFData = [ _mainWindow dataWithPDFInsideRect: NSMakeRect( 0, 0, frame.size.width, frame.size.height ) ];
+
+    NSSavePanel* savePanel = [ NSSavePanel savePanel ];
+
+    [ savePanel beginSheetModalForWindow: _mainWindow
+                       completionHandler:
+        ^( NSInteger _Result )
+            {
+            [ PDFData writeToURL: [ savePanel URL ] atomically: YES ];
+            } ];
+    }
+
+- ( IBAction ) displayWindow: ( id )_Sender
+    {
+    [ _mainWindow display ];
     }
 
 @end // BLAppDelegate
