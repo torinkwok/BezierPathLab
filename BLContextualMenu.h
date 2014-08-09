427b17e5a4cc6b299a6efbdddebfebbb3c82c461
Tong Guo <Tong-G@outlook.com>
2014-08-10 01:26:42 +0800

    Essentially completed  to fuck <Application Menu and Pop-Up List Programming Topics>.

--- BLContextualMenu.h
+++ BLContextualMenu.h
@@ -31,90 +31,27 @@
  **                                                                         **
  ****************************************************************************/
 
-#import <Cocoa/Cocoa.h>
+#import <Foundation/Foundation.h>
 
-// Constants Definitions
-enum BLPathType { BLPathTypeSquare = 0, BLPathTypeCircle, BLPathTypeArc, BLPathTypeLine };
+@interface BLContextualMenu : NSMenu
 
-enum BLLineCapStyle { BLLineCapStyleButtLine = 0, BLLineCapStyleSquareLine, BLLineCapStyleRoundLine };
+@property ( assign ) IBOutlet NSMenuItem* _squareMenuItem;
+@property ( assign ) IBOutlet NSMenuItem* _circleMenuItem;
+@property ( assign ) IBOutlet NSMenuItem* _arcMenuItem;
+@property ( assign ) IBOutlet NSMenuItem* _lineMenuItem;
 
-enum BLDashStyle { BLDashTyleSolid = 0, BLDashTyle5_5, BLDashTyle8_3_8, BLDashTyle9_6_3 };
+@property ( assign ) IBOutlet NSMenuItem* _filledMenuItem;
 
-// Keys for Preferences
-NSString extern* const BLUserDefaultsKeyLineColor;
-NSString extern* const BLUserDefaultsKeyFillColor;
-NSString extern* const BLUserDefaultsKeyBackgroundColor;
+@property ( assign ) IBOutlet NSMenuItem* _buttLineMenuItem;
+@property ( assign ) IBOutlet NSMenuItem* _roundLineMenuItem;
+@property ( assign ) IBOutlet NSMenuItem* _squareLineMenuItem;
 
-NSString extern* const BLUserDefaultsKeyAngle;
-NSString extern* const BLUserDefaultsKeyZoom;
-NSString extern* const BLUserDefaultsKeyLineWidth;
+@property ( assign ) IBOutlet NSMenuItem* _solidMenuItem;
+@property ( assign ) IBOutlet NSMenuItem* _5_5MenuItem;
+@property ( assign ) IBOutlet NSMenuItem* _8_3_8MenuItem;
+@property ( assign ) IBOutlet NSMenuItem* _9_6_3MenuItem;
 
-NSString extern* const BLUserDefaultsKeyIsFilled;
-
-NSString extern* const BLUserDefaultsKeyPathType;
-NSString extern* const BLUserDefaultsKeyLineCapStyle;
-NSString extern* const BLUserDefaultsKeyDashStyle;
-
-NSString extern* const BLUserDefaultsKeyShapeLocation;
-
-NSString extern* const BLUserDefaultsKeyKeyEquivalent;
-NSString extern* const BLUserDefaultsKeyKeyEquivalentModifier;
-
-// BLDashboardView class
-@interface BLDashboardView : NSView <NSUserInterfaceValidations, NSMenuDelegate>
-    {
-    NSInteger _dashCount;
-    CGFloat _dashArray[ 3 ];
-    }
-
-#pragma mark Outlets
-@property ( assign ) IBOutlet NSColorWell* _lineColorWell;
-@property ( assign ) IBOutlet NSColorWell* _fillColorWell;
-@property ( assign ) IBOutlet NSColorWell* _backgroundColorWell;
-
-@property ( assign ) IBOutlet NSMatrix* _pathTypeMatrix;
-@property ( assign ) IBOutlet NSMatrix* _lineCapStyleMatrix;
-@property ( assign ) IBOutlet NSMatrix* _dashTypeMatrix;
-
-@property ( assign ) IBOutlet NSButton* _isFilledButton;
-
-@property ( assign ) IBOutlet NSSlider* _angleSlider;
-@property ( assign ) IBOutlet NSSlider* _zoomSlider;
-@property ( assign ) IBOutlet NSSlider* _lineWidthSlider;
-
-#pragma mark Instance variables
-@property ( retain ) NSBezierPath* _bezierPath;
-
-@property ( assign ) enum BLPathType _pathType;
-@property ( assign ) enum BLLineCapStyle _lineCapStyle;
-@property ( assign ) enum BLDashStyle _dashStyle;
-
-@property ( assign ) BOOL _isFilled;
-
-@property ( assign ) CGFloat _angle;
-@property ( assign ) CGFloat _zoom;
-@property ( assign ) CGFloat _lineWidth;
-
-@property ( retain ) NSColor* _lineColor;
-@property ( retain ) NSColor* _fillColor;
-@property ( retain ) NSColor* _backgroundColor;
-
-#pragma mark IBActions
-- ( IBAction ) changedPathType: ( id )_Sender;
-- ( IBAction ) changedLineCapStyle: ( id )_Sender;
-- ( IBAction ) changedDashType: ( id )_Sender;
-
-- ( IBAction ) changedFilled: ( id ) _Sender;
-
-- ( IBAction ) changedLineColor: ( id )_Sender;
-- ( IBAction ) changedFillColor: ( id )_Sender;
-- ( IBAction ) changedBackgroundColor: ( id )_Sender;
-
-- ( IBAction ) changedAngle: ( id )_Sender;
-- ( IBAction ) changedZoom: ( id )_Sender;
-- ( IBAction ) changeLineWidth: ( id )_Sender;
-
-@end // BLDashboardView
+@end
 
 /////////////////////////////////////////////////////////////////////////////
 
--- BLContextualMenu.m
+++ BLContextualMenu.m
@@ -31,90 +31,34 @@
  **                                                                         **
  ****************************************************************************/
 
-#import <Cocoa/Cocoa.h>
+#import "BLContextualMenu.h"
+#import "BLDashboardView.h"
 
-// Constants Definitions
-enum BLPathType { BLPathTypeSquare = 0, BLPathTypeCircle, BLPathTypeArc, BLPathTypeLine };
+@implementation BLContextualMenu
 
-enum BLLineCapStyle { BLLineCapStyleButtLine = 0, BLLineCapStyleSquareLine, BLLineCapStyleRoundLine };
+@synthesize _squareMenuItem;
+@synthesize _circleMenuItem;
+@synthesize _arcMenuItem;
+@synthesize _lineMenuItem;
 
-enum BLDashStyle { BLDashTyleSolid = 0, BLDashTyle5_5, BLDashTyle8_3_8, BLDashTyle9_6_3 };
+@synthesize _filledMenuItem;
 
-// Keys for Preferences
-NSString extern* const BLUserDefaultsKeyLineColor;
-NSString extern* const BLUserDefaultsKeyFillColor;
-NSString extern* const BLUserDefaultsKeyBackgroundColor;
+@synthesize _buttLineMenuItem;
+@synthesize _roundLineMenuItem;
+@synthesize _squareLineMenuItem;
 
-NSString extern* const BLUserDefaultsKeyAngle;
-NSString extern* const BLUserDefaultsKeyZoom;
-NSString extern* const BLUserDefaultsKeyLineWidth;
+@synthesize _solidMenuItem;
+@synthesize _5_5MenuItem;
+@synthesize _8_3_8MenuItem;
+@synthesize _9_6_3MenuItem;
 
-NSString extern* const BLUserDefaultsKeyIsFilled;
-
-NSString extern* const BLUserDefaultsKeyPathType;
-NSString extern* const BLUserDefaultsKeyLineCapStyle;
-NSString extern* const BLUserDefaultsKeyDashStyle;
-
-NSString extern* const BLUserDefaultsKeyShapeLocation;
-
-NSString extern* const BLUserDefaultsKeyKeyEquivalent;
-NSString extern* const BLUserDefaultsKeyKeyEquivalentModifier;
-
-// BLDashboardView class
-@interface BLDashboardView : NSView <NSUserInterfaceValidations, NSMenuDelegate>
+#pragma mark Conforms <NSNibAwaking> protocol
+- ( void ) awakeFromNib
     {
-    NSInteger _dashCount;
-    CGFloat _dashArray[ 3 ];
+    [ self._filledMenuItem setState: [ USER_DEFAULTS boolForKey: BLUserDefaultsKeyIsFilled ] ];
     }
 
-#pragma mark Outlets
-@property ( assign ) IBOutlet NSColorWell* _lineColorWell;
-@property ( assign ) IBOutlet NSColorWell* _fillColorWell;
-@property ( assign ) IBOutlet NSColorWell* _backgroundColorWell;
-
-@property ( assign ) IBOutlet NSMatrix* _pathTypeMatrix;
-@property ( assign ) IBOutlet NSMatrix* _lineCapStyleMatrix;
-@property ( assign ) IBOutlet NSMatrix* _dashTypeMatrix;
-
-@property ( assign ) IBOutlet NSButton* _isFilledButton;
-
-@property ( assign ) IBOutlet NSSlider* _angleSlider;
-@property ( assign ) IBOutlet NSSlider* _zoomSlider;
-@property ( assign ) IBOutlet NSSlider* _lineWidthSlider;
-
-#pragma mark Instance variables
-@property ( retain ) NSBezierPath* _bezierPath;
-
-@property ( assign ) enum BLPathType _pathType;
-@property ( assign ) enum BLLineCapStyle _lineCapStyle;
-@property ( assign ) enum BLDashStyle _dashStyle;
-
-@property ( assign ) BOOL _isFilled;
-
-@property ( assign ) CGFloat _angle;
-@property ( assign ) CGFloat _zoom;
-@property ( assign ) CGFloat _lineWidth;
-
-@property ( retain ) NSColor* _lineColor;
-@property ( retain ) NSColor* _fillColor;
-@property ( retain ) NSColor* _backgroundColor;
-
-#pragma mark IBActions
-- ( IBAction ) changedPathType: ( id )_Sender;
-- ( IBAction ) changedLineCapStyle: ( id )_Sender;
-- ( IBAction ) changedDashType: ( id )_Sender;
-
-- ( IBAction ) changedFilled: ( id ) _Sender;
-
-- ( IBAction ) changedLineColor: ( id )_Sender;
-- ( IBAction ) changedFillColor: ( id )_Sender;
-- ( IBAction ) changedBackgroundColor: ( id )_Sender;
-
-- ( IBAction ) changedAngle: ( id )_Sender;
-- ( IBAction ) changedZoom: ( id )_Sender;
-- ( IBAction ) changeLineWidth: ( id )_Sender;
-
-@end // BLDashboardView
+@end
 
 /////////////////////////////////////////////////////////////////////////////
 
